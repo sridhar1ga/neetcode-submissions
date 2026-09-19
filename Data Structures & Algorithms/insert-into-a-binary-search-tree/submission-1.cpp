f7bc2737b8030b345f93c86367ca0296accbb726
Sridhar Vanga <sridharvanga@outlook.com>
2026-09-19 19:13:50 +0530

    Add: insert-into-a-binary-search-tree - submission-1

---
+++ Data Structures & Algorithms/insert-into-a-binary-search-tree/submission-1.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        TreeNode* main = root;
+        if(root==NULL) return new TreeNode(val);
+        while(root!=nullptr)
+        {
+            if(root->val>val)
+            {
+                if(root->left==NULL) {
+                    root->left = new TreeNode(val);
+                    break;
+                }
+                root = root->left;
+            }
+            else{
+                if(root->right==NULL){
+                    root->right = new TreeNode(val);
+                    break;
+                }
+                root = root->right;
+            }
+        }
+
+        return main;
+
+    }
+};
